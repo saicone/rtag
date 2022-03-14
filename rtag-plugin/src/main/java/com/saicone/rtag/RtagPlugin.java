4a9d39f8a8feb49875542d377374aeea847f8327
Rubenicos <44579213+Rubenicos@users.noreply.github.com>
2022-03-14 11:16:46 -0600

    Class loading on start

--- rtag-plugin/src/main/java/com/saicone/rtag/RtagPlugin.java
+++ rtag-plugin/src/main/java/com/saicone/rtag/RtagPlugin.java
@@ -13,4 +13,34 @@ public class RtagPlugin extends JavaPlugin {
     public RtagPlugin() {
         instance = this;
     }
+
+    @Override
+    public void onLoad() {
+        // Utils
+        loadRtagClass("util.EasyLookup",
+                "util.ChatComponent",
+                "util.EnchantmentTag",
+                "util.ItemMaterialTag",
+                "util.SkullTexture");
+        // Main
+        loadRtagClass("Rtag", "RtagMirror");
+        // Tag
+        loadRtagClass("tag.TagBase", "tag.TagList", "tag.TagCompound");
+        // Stream
+        loadRtagClass("stream.TStream", "stream.TStreamTools");
+        // Block
+        loadRtagClass("block.TileBridge", "block.TileTag");
+        // Entity
+        loadRtagClass("entity.EntityBridge", "entity.EntityTag");
+        // Item
+        loadRtagClass("item.ItemBridge", "item.ItemTag", "item.ItemTagStream");
+    }
+
+    private void loadRtagClass(String... names) {
+        for (String name : names) {
+            try {
+                Class.forName("com.saicone.rtag." + name);
+            } catch (ClassNotFoundException ignored) { }
+        }
+    }
 }
